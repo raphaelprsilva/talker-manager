493031131f66aa8a23e391b3c398faec2b49f38f
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-11-22 09:53:07 -0300

    feat: ensure is possible get `talker` by id

--- src/routes/talkersRouter.js
+++ src/routes/talkersRouter.js
@@ -8,4 +8,17 @@ router.get('/', async (_request, response) => {
   response.status(200).json(talkers);
 });
 
+router.get('/:id', async (request, response) => {
+  const { id } = request.params;
+  const talkers = await readTalkerFile();
+  const talker = talkers.find((t) => t.id === Number(id));
+
+  if (!talker) {
+    return response
+      .status(404)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  response.status(200).json(talker);
+});
+
 module.exports = router;
